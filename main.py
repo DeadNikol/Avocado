c8f4661110c84f0e869af11b927d0e6219c4e8a7
DeadNikol <121517278+DeadNikol@users.noreply.github.com>
2022-12-28 21:32:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,18 @@ for i in range(len(y)):
     else:
         z.append(x%q)
         z.reverse()
+        for i in range(len(z)):
+            if z[i]==10:
+                z[i]='A'
+            elif z[i]==11:
+                z[i]='B'
+            elif z[i]==12:
+                z[i]='C'
+            elif z[i]==13:
+                z[i]='D'
+            elif z[i]==14:
+                z[i]='E'
+            elif z[i]==15:
+                z[i]='F'
         print(z)
         break
