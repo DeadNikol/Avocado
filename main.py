1713d7bc5342964d6505f53f37472df7d0281910
DeadNikol <121517278+DeadNikol@users.noreply.github.com>
2022-12-28 22:51:41 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,9 @@
-x = int(input())
-q=int(input())
-y=list(range(100))
-z=list()
-for i in range(len(y)):
-    if x/q>=1:
-        z.append(x%q)
-        x=x//q
-    else:
-        z.append(x%q)
-        z.reverse()
-        for i in range(len(z)):
-            if z[i]==10:
-                z[i]='A'
-            elif z[i]==11:
-                z[i]='B'
-            elif z[i]==12:
-                z[i]='C'
-            elif z[i]==13:
-                z[i]='D'
-            elif z[i]==14:
-                z[i]='E'
-            elif z[i]==15:
-                z[i]='F'
-        print(z)
-        break
+import Nepolicarbonat
+import Policarbonat
+x = int(input('1 если из 10 в x, 2 если из x в 10: '))
+if x == 1:
+    Policarbonat.From10()
+elif x == 2:
+    Nepolicarbonat.To10()
+else:
+    print("Пошёл нахер, нет такого функционала")
