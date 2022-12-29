c95576577effff1440ff3eaaf04cf5a0e27d03af
DeadNikol <121517278+DeadNikol@users.noreply.github.com>
2022-12-29 13:48:32 +0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import Nepolicarbonat
-import Policarbonat
-x = int(input('1 если из 10 в x, 2 если из x в 10: '))
-if x == 1:
-    Policarbonat.From10()
-elif x == 2:
-    Nepolicarbonat.To10()
-else:
-    print("Пошёл нахер, нет такого функционала")
